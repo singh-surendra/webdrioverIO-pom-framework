7551ee19b3cee18ed64154ac0d49d6eac5b3498c
singh-surendra <surendrasingh.db@gmail.com>
2020-04-17 14:34:34 +0200

    disabling suite files

--- test/sanitySuite/home.test.js
+++ test/sanitySuite/home.test.js
@@ -1,28 +1,28 @@
-const loginPage = require("/Users/surendra.singh/Documents/Automation/webdriverio-learning/webdriverio-app-POM-Framework/pages/login.page.js");
-const homePage = require("/Users/surendra.singh/Documents/Automation/webdriverio-learning/webdriverio-app-POM-Framework/pages/home.page.js");
-const configData = require("/Users/surendra.singh/Documents/Automation/webdriverio-learning/webdriverio-app-POM-Framework/config.js")
-const constants = require("/Users/surendra.singh/Documents/Automation/webdriverio-learning/webdriverio-app-POM-Framework/constants.js")
+// const loginPage = require("/Users/surendra.singh/Documents/Automation/webdriverio-learning/webdriverio-app-POM-Framework/pages/login.page.js");
+// const homePage = require("/Users/surendra.singh/Documents/Automation/webdriverio-learning/webdriverio-app-POM-Framework/pages/home.page.js");
+// const configData = require("/Users/surendra.singh/Documents/Automation/webdriverio-learning/webdriverio-app-POM-Framework/config.js")
+// const constants = require("/Users/surendra.singh/Documents/Automation/webdriverio-learning/webdriverio-app-POM-Framework/constants.js")
 
-describe('home page feature test', function () {
-    //retry logic for all "it" blocks
-    // this.retries(3)
-    it('verify home page header text', function () {
-        //overwrite the retry logic for specific test case
-        this.retries(2)
-        browser.url('/')
-        browser.maximizeWindow();
-        loginPage.doLogin(configData.username, configData.password)
-        const headerText = homePage.getPageHeaderText()
-        console.log('getting started page header text is', headerText);
-        assert.equal(constants.HOME_PAGE_GETTING_STARTED_HEADER_TITLE, headerText, 'getting started header is not found')
-    })
+// describe('home page feature test', function () {
+//     //retry logic for all "it" blocks
+//     // this.retries(3)
+//     it('verify home page header text', function () {
+//         //overwrite the retry logic for specific test case
+//         this.retries(2)
+//         browser.url('/')
+//         browser.maximizeWindow();
+//         loginPage.doLogin(configData.username, configData.password)
+//         const headerText = homePage.getPageHeaderText()
+//         console.log('getting started page header text is', headerText);
+//         assert.equal(constants.HOME_PAGE_GETTING_STARTED_HEADER_TITLE, headerText, 'getting started header is not found')
+//     })
 
-    it('verify logged in account email', function () {
-        browser.url('/')
-        browser.maximizeWindow();
-        loginPage.doLogin(configData.username, configData.password)
-        homePage.doClickAccountMenu()
-        const email = homePage.getLoggedInAccountEmail()
-        assert.equal(configData.username, email, 'email address is not found')
-    })
-})
+//     it('verify logged in account email', function () {
+//         browser.url('/')
+//         browser.maximizeWindow();
+//         loginPage.doLogin(configData.username, configData.password)
+//         homePage.doClickAccountMenu()
+//         const email = homePage.getLoggedInAccountEmail()
+//         assert.equal(configData.username, email, 'email address is not found')
+//     })
+// })
--- test/smokeSuite/login.test.js
+++ test/smokeSuite/login.test.js
@@ -1,29 +1,29 @@
-const loginPage = require("/Users/surendra.singh/Documents/Automation/webdriverio-learning/webdriverio-app-POM-Framework/pages/login.page.js");
-const configData = require("/Users/surendra.singh/Documents/Automation/webdriverio-learning/webdriverio-app-POM-Framework/config.js")
-const constants = require("/Users/surendra.singh/Documents/Automation/webdriverio-learning/webdriverio-app-POM-Framework/constants.js")
-describe('login page feature test', function(){
-    //retry logic
-    // this.retries(2)
-    it('verify login page title',function(){
-         browser.url('/')
-         browser.maximizeWindow();
-         const title = loginPage.getPageTitle()
-         console.log('login page title is', title);
-         assert.equal(constants.LOGIN_PAGE_TITLE, title, 'title is not found error')
-    })
+// const loginPage = require("/Users/surendra.singh/Documents/Automation/webdriverio-learning/webdriverio-app-POM-Framework/pages/login.page.js");
+// const configData = require("/Users/surendra.singh/Documents/Automation/webdriverio-learning/webdriverio-app-POM-Framework/config.js")
+// const constants = require("/Users/surendra.singh/Documents/Automation/webdriverio-learning/webdriverio-app-POM-Framework/constants.js")
+// describe('login page feature test', function(){
+//     //retry logic
+//     // this.retries(2)
+//     it('verify login page title',function(){
+//          browser.url('/')
+//          browser.maximizeWindow();
+//          const title = loginPage.getPageTitle()
+//          console.log('login page title is', title);
+//          assert.equal(constants.LOGIN_PAGE_TITLE, title, 'title is not found error')
+//     })
 
-    it('verify signup link',function(){
-        browser.url('/')
-        browser.maximizeWindow();
-        loginPage.isSignUpLinkExist()
-        assert.equal(true, loginPage.isSignUpLinkExist(), 'sign up link is not present')
-   })
+//     it('verify signup link',function(){
+//         browser.url('/')
+//         browser.maximizeWindow();
+//         loginPage.isSignUpLinkExist()
+//         assert.equal(true, loginPage.isSignUpLinkExist(), 'sign up link is not present')
+//    })
 
-   it('verify login',function(){
-    browser.url('/')
-    browser.maximizeWindow();
-    loginPage.doLogin(configData.username, configData.password)
+//    it('verify login',function(){
+//     browser.url('/')
+//     browser.maximizeWindow();
+//     loginPage.doLogin(configData.username, configData.password)
 
-})
+// })
 
-})
+// })
